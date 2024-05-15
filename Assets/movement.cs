0c0adcb2375494f4f88ab5aace24355a295a852d
PetabyteBrain <153134032+PetabyteBrain@users.noreply.github.com>
2024-05-15 22:06:32 +0200

    Added square

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -7,7 +7,7 @@ public class movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //s
     }
 
     // Update is called once per frame
